835085e065caad8ec5b21fcc890d426ec1128f3e
eskander-taher <eskander.alshaibany@gmail.com>
2023-04-16 23:37:19 +0500

    new static files

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const env = require("dotenv").config();
 const colors = require("colors");
 const port = process.env.PORT || 5000;
-
+const path = require("path");
 const connectDB = require("./config/connectDB");
 connectDB(process.env.MONGO_URI);
 
@@ -17,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("storage"));
+app.use(express.static("static"));
 
 app.use(logger);
 
@@ -31,4 +32,8 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 
 app.use(errorHandler);
 
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "static/index.html"));
+});
 app.listen(port, () => console.log(`listening on port ${port}`));
